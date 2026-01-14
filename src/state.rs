4c482a16112301abccc315ab9b4203b93fc6eb73
Liam <liam@scalzulli.com>
2026-01-14 00:11:34 -0500

    Add unit tests for `State` handler

--- src/state.rs
+++ src/state.rs
@@ -49,3 +49,90 @@ impl State {
       })
   }
 }
+
+#[cfg(all(test, unix))]
+mod tests {
+  use {super::*, std::os::unix::fs::symlink, tempfile::TempDir};
+
+  #[test]
+  fn source_missing() {
+    let directory = TempDir::new().unwrap();
+
+    assert_eq!(
+      State::get(
+        &directory.path().join("nonexistent"),
+        &directory.path().join("link")
+      ),
+      State::SourceMissing
+    );
+  }
+
+  #[test]
+  fn target_missing() {
+    let directory = TempDir::new().unwrap();
+
+    let source = directory.path().join("source");
+
+    fs::write(&source, "content").unwrap();
+
+    assert_eq!(State::get(&source, &directory.path().join("link")), State::Missing);
+  }
+
+  #[test]
+  fn conflict_with_file() {
+    let directory = TempDir::new().unwrap();
+
+    let source = directory.path().join("source");
+    let target = directory.path().join("target");
+
+    fs::write(&source, "source content").unwrap();
+    fs::write(&target, "target content").unwrap();
+
+    assert_eq!(State::get(&source, &target), State::Conflict);
+  }
+
+  #[test]
+  fn conflict_with_directory() {
+    let directory = TempDir::new().unwrap();
+
+    let source = directory.path().join("source");
+    let target = directory.path().join("target");
+
+    fs::write(&source, "content").unwrap();
+    fs::create_dir(&target).unwrap();
+
+    assert_eq!(State::get(&source, &target), State::Conflict);
+  }
+
+  #[test]
+  fn linked() {
+    let directory = TempDir::new().unwrap();
+
+    let source = directory.path().join("source");
+    let target = directory.path().join("link");
+
+    fs::write(&source, "content").unwrap();
+    symlink(&source, &target).unwrap();
+
+    assert_eq!(State::get(&source, &target), State::Linked);
+  }
+
+  #[test]
+  fn misdirected() {
+    let directory = TempDir::new().unwrap();
+
+    let source = directory.path().join("source");
+    let other = directory.path().join("other");
+    let target = directory.path().join("link");
+
+    fs::write(&source, "content").unwrap();
+    fs::write(&other, "other content").unwrap();
+
+    symlink(&other, &target).unwrap();
+
+    assert_eq!(
+      State::get(&source, &target),
+      State::Misdirected { actual: other }
+    );
+  }
+}
